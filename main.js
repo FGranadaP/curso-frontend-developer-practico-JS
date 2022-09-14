336a0f74142d227abfe11ab668a24365e70a54c0
Felipe Granada <fgranadap@gmail.com>
2022-09-13 21:00:28 -0500

    Se integran la navbar y el menu desplegable

---
+++ main.js
@@ -0,0 +1,23 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    // if(desktopMenu.classList.contains('inactive')){
+    //     desktopMenu.classList.remove('inactive');
+    // }else{
+    //     desktopMenu.classList.add('inactive');
+    // }
+    desktopMenu.classList.toggle('inactive');   
+}
+console.log("Si se ejecuto JS")
+
+
+function toggleDesktopMenu(){
+    if(desktopMenu.classList.contains('inactive')){
+        desktopMenu.classList.remove('inactive');
+    }else{
+        desktopMenu.classList.add('inactive');
+    }  
+}
