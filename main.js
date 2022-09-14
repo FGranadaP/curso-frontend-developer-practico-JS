7bc9fa8c568817700ec10e08c033f49d0d929564
Felipe Granada <fgranadap@gmail.com>
2022-09-14 15:23:27 -0500

    Se agrega el aside de carrito y se modifica JS en sus toggle

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
     // if(desktopMenu.classList.contains('inactive')){
@@ -12,11 +16,20 @@ function toggleDesktopMenu(){
     // }else{
     //     desktopMenu.classList.add('inactive');
     // }
-    desktopMenu.classList.toggle('inactive');   
+    aside.classList.add('inactive')
+    desktopMenu.classList.toggle('inactive');
+
 }
 
 function toggleMobileMenu(){
+    aside.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');   
+}
+
+function toggleCarritoAside(){
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
+    aside.classList.toggle('inactive');   
 }
 
 
